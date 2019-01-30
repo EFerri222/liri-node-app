8d6cdca0724659bee30b8e353b7adbce23c6765b
EFerri222 <eferri222@gmail.com>
2019-01-30 03:15:17 -0500

    added functionality for the terminal command 'node liri.js spotify-this-song <song name here>' to search the Spotify API for a song and render the artist(s) of the song, song name, preview link from Spotify, and the album that the song is from to the terminal

--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
 
 var Concert = require("./concerts");
+var Spotify = require('node-spotify-api');
 
 // Create a new Concert object
 var concert = new Concert();
+
+// Create a new Spotify object
+var spotify = new Spotify(keys.spotify);
 
 var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
@@ -13,4 +16,31 @@ var term = process.argv.slice(3).join(" ");
 if (search === "concert-this") {
     console.log(term.charAt(0).toUpperCase() + term.slice(1) + " Concerts:");
     concert.findConcert(term);
+} else if (search === "spotify-this-song") {
+
+    if (!term) {
+        term = "the sign ace of base";
+    }
+    
+    spotify.search({type:'track', query:term}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+
+        var jsonData = data.tracks.items[0];
+        var artists = [];
+
+        for (var i=0; i < jsonData.artists.length; i++) {
+            artists.push(jsonData.artists[i].name);
+        }
+
+        var songData = [
+            "Artist(s): " + artists.join(", "),
+            "Song Name: " + jsonData.name,
+            "Preview URL: " + jsonData.external_urls.spotify,
+            "Album: " + jsonData.album.name
+        ].join("\n");
+
+        console.log(songData);
+    });
 }
